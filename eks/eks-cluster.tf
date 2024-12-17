b2666f8a3e5fb78c16ca3b7726382e34ef80dc74
devopseasylearning <info@devopseasylearning.com>
2024-12-17 18:36:55 -0500

    eks-guru-working

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -3,7 +3,17 @@ module "eks" {
   version = "19.0.4"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.24"
+  #cluster_version = "1.24"
+  cluster_version = "1.30"
+
+
+  # EKS Addons
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -24,15 +34,15 @@ module "eks" {
       max_size     = 3
       desired_size = 2
     }
+    
+    # two = {
+    #   name = "node-group-2"
 
-    two = {
-      name = "node-group-2"
+    #   instance_types = ["t3.small"]
 
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
+    #   min_size     = 1
+    #   max_size     = 2
+    #   desired_size = 1
+    # }
   }
 }
